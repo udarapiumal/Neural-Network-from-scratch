cf4d9042ef3b95623610d8857d12fec1dfe40d3d
udarapiumal <udarapiumal0@gmail.com>
2025-07-29 21:32:34 +0530

    neuron

---
+++ neuron.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def sigmoid(x):
+    return (1/(1+np.exp(-x)))
+
+class neuron:
+    def __init__(self,weights,bias):
+        self.weights=weights
+        self.bias=bias
+        
+    
+    def feedforward(self,inputs):
+        total=np.dot(self.weights,inputs) + self.bias
+        return sigmoid(total)
+    
+    
+weights=np.array([0,1])
+bias=4
+n=neuron(weights,bias)
+
+inputs=np.array([2,3])
+print(n.feedforward(inputs))
+
+        
+        
